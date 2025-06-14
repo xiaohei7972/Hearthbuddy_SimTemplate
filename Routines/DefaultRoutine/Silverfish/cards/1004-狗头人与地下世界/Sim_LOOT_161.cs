9c74c21f95b4de7fc6394a5cb87b12b82b7748b5
xiaohei7972 <1901810053@qq.com>
2025-06-15 06:40:23 +0800

    食肉魔块

--- Routines/DefaultRoutine/Silverfish/cards/1004-狗头人与地下世界/Sim_LOOT_161.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1004-狗头人与地下世界/Sim_LOOT_161.cs
@@ -4,20 +4,54 @@ using System.Text;
 
 namespace HREngine.Bots
 {
-	class Sim_LOOT_161 : SimTemplate //* 食肉魔块 Carnivorous Cube
-	{
-		//<b>Battlecry:</b> Destroya friendly minion.<b>Deathrattle:</b> Summon 2 copies of it.
-		//<b>战吼：</b>消灭一个友方随从。<b>亡语：</b>召唤两个被消灭随从的复制。
-		
-		
+    class Sim_LOOT_161 : SimTemplate //* 食肉魔块 Carnivorous Cube
+    {
+        //<b>Battlecry:</b> Destroya friendly minion.<b>Deathrattle:</b> Summon 2 copies of it.
+        //<b>战吼：</b>消灭一个友方随从。<b>亡语：</b>召唤两个被消灭随从的复制。
+
+        CardDB.Card kid = null;
+        public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+        {
+            if (p.ownMinions.Count != 0)
+            {
+                p.minionGetDestroyed(target);
+                kid = CardDB.Instance.getCardDataFromID(target.name);
+            }
+        }
+
+
+
+        public override void onDeathrattle(Playfield p, Minion m)
+        {
+            if (kid == null) return;
+
+
+            int pos = m.own ? p.ownMinions.Count : p.enemyMinions.Count;
+            p.callKid(kid, pos, true);
+
+        }
+
+        public override PlayReq[] GetPlayReqs()
+        {
+            return new PlayReq[]{
+                new PlayReq(CardDB.ErrorType2.REQ_TARGET_TO_PLAY), // 需要选择一个目标
+				new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET), // 目标只能是随从
+				new PlayReq(CardDB.ErrorType2.REQ_FRIENDLY_TARGET), // 目标只能是友方
+				new PlayReq(CardDB.ErrorType2.REQ_TARGET_IF_AVAILABLE) // 没有目标时也可以使用
+
+			};
+
+        }
+
 
         public override PlayReq[] GetPlayReqs()
         {
             return new PlayReq[] {
-                new PlayReq(CardDB.ErrorType2.REQ_TARGET_IF_AVAILABLE),
-                new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET),
-                new PlayReq(CardDB.ErrorType2.REQ_FRIENDLY_TARGET),
+                new PlayReq(CardDB.ErrorType2.REQ_TARGET_TO_PLAY), // 需要选择一个目标
+                new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET), // 目标只能是随从
+                new PlayReq(CardDB.ErrorType2.REQ_FRIENDLY_TARGET), // 目标只能是友方
+                new PlayReq(CardDB.ErrorType2.REQ_TARGET_IF_AVAILABLE) // 没有目标时也可以使用
             };
         }
-	}
+    }
 }
