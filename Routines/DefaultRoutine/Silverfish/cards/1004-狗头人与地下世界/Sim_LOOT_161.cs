79ecb5b242387de586a7fbb9d255480c46b3e65a
xiaohei7972 <1901810053@qq.com>
2025-06-15 06:49:19 +0800

    食肉魔块

--- Routines/DefaultRoutine/Silverfish/cards/1004-狗头人与地下世界/Sim_LOOT_161.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1004-狗头人与地下世界/Sim_LOOT_161.cs
@@ -42,15 +42,5 @@ namespace HREngine.Bots
 
         }
 
-
-        public override PlayReq[] GetPlayReqs()
-        {
-            return new PlayReq[] {
-                new PlayReq(CardDB.ErrorType2.REQ_TARGET_TO_PLAY), // 需要选择一个目标
-                new PlayReq(CardDB.ErrorType2.REQ_MINION_TARGET), // 目标只能是随从
-                new PlayReq(CardDB.ErrorType2.REQ_FRIENDLY_TARGET), // 目标只能是友方
-                new PlayReq(CardDB.ErrorType2.REQ_TARGET_IF_AVAILABLE) // 没有目标时也可以使用
-            };
-        }
     }
 }
