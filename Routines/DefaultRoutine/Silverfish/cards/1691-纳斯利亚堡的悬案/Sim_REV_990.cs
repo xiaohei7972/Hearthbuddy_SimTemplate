e646a0eb98bd7567c8b8712e4c0dfb28748ada9d
xiaohei7972 <1901810053@qq.com>
2025-06-30 13:19:57 +0800

    update

--- Routines/DefaultRoutine/Silverfish/cards/1691-纳斯利亚堡的悬案/Sim_REV_990.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1691-纳斯利亚堡的悬案/Sim_REV_990.cs
@@ -4,20 +4,23 @@ using System.Text;
 
 namespace HREngine.Bots
 {
-	//地标 战士 费用：1
-	//Sanguine Depths
-	//赤红深渊
-	//[x]Deal 1 damage to a minion and give it +2 Attack.
-	//对一个随从造成1点伤害，并使其获得+2攻击力。
-	class Sim_REV_990 : SimTemplate
-	{
+    //地标 战士 费用：1
+    //Sanguine Depths
+    //赤红深渊
+    //[x]Deal 1 damage to a minion and give it +2 Attack.
+    //对一个随从造成1点伤害，并使其获得+2攻击力。
+    class Sim_REV_990 : SimTemplate
+    {
         public override void useLocation(Playfield p, Minion triggerMinion, Minion target)
         {
-            // 对目标随从造成1点伤害
-            p.minionGetDamageOrHeal(target, 1);
+            if (target != null)
+            {
+                // 对目标随从造成1点伤害
+                p.minionGetDamageOrHeal(target, 1);
 
-            // 使目标随从获得+2攻击力（永久）
-            p.minionGetBuffed(target, 2, 0); // 给予永久的攻击力加成
+                // 使目标随从获得+2攻击力（永久）
+                p.minionGetBuffed(target, 2, 0); // 给予永久的攻击力加成
+            }
         }
 
         public override PlayReq[] GetUseAbilityReqs()
